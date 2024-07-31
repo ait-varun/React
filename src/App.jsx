d27b545fb492b21f921b898422e8f4d9cefc73c0
Varun <aitvarunbhatia@gmail.com>
2024-07-31 09:42:36 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/home";
 import About from "./pages/About/about";
+import Nav from "./components/layout/nav";
 
-function App() {
+export default function App() {
   return (
     <>
       <Router>
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -15,5 +17,3 @@ function App() {
     </>
   );
 }
-
-export default App;
